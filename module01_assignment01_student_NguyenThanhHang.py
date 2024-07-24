da3706e2bfb603e8546d5b57812f98da8ede4230
Nguyen-Thanh-Hang <grazie.thanh.hang@gmail.com>
2024-07-24 16:08:08 +0700

    module01_assignment01_student_NguyenThanhHang.py

--- module01_assignment01_student_NguyenThanhHang.py
+++ module01_assignment01_student_NguyenThanhHang.py
@@ -1,3 +1,13 @@
+#In ra dòng Hello World
 a= "Hello World!"
 print(a)
+#Hello World!
+
+#Làm quen với biến
+a = 'Hello World!'
+print(a)
+#Hello World 
+#Gán chuỗi Hello World bằng biến a
+#Dùng hàm print in ra biến đó.
+
 import this
