a99185813c6447f604e56b6589ae60074a68d0d3
Nguyen-Thanh-Hang <grazie.thanh.hang@gmail.com>
2024-07-26 14:59:08 +0700

    Update and rename module01_assignment01_student_NguyenThanhHang.py to module01_assignment01_student02_NguyenThanhHang.py

---
+++ module01_assignment01_student02_NguyenThanhHang.py
@@ -0,0 +1,8 @@
+Bài tập 1. Python
+(a) Ngôn ngữ lập trình Python được tạo ra bởi ai và vào thời gian nào? (https://glints.com/vn/blog/ngon-ngu-lap-trinh-python-la-gi/)
+- Ngôn ngữ lập trình Python tạo bởi Guido Van Rossum (Lập trình viên người Hà Lan) vào cuối những năm 1980
+(b) Vì sao ngôn ngữ lập trình Python lại được đặt tên như vậy?
+(https://glints.com/vn/blog/ngon-ngu-lap-trinh-python-la-gi/)
+-  Van Rossum đã chọn tên Python cho ngôn ngữ mới từ một chương trình truyền hình, Monty Python’s Flying Circus.
+(c) Tính tới thời điểm hiện tại, phiên bản Python mới nhất được phát hành là phiên bản nào? (https://www.python.org/downloads/)
+- Tính đến thời điểm hiện tại, phiên bản Python 3.12.4 là phiên bản được phát hành mới nhất 
