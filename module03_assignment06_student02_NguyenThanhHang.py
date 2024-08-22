17d782d70bc18d111982891768fffedb30041874
Nguyen-Thanh-Hang <grazie.thanh.hang@gmail.com>
2024-08-22 17:43:52 +0700

    Add files via upload

---
+++ module03_assignment06_student02_NguyenThanhHang.py
@@ -0,0 +1,31 @@
+import random
+def ucln_euclid(num1 ,num2):
+    if (num2 == 0):
+      return num1
+#Nếu num2 khác 0 thì tìm UCLN của num2 và số dư của num1 cho num2
+    else:
+        return ucln_euclid(num2, num1 % num2)
+    # Lấy ngẫu nhiên hai số
+N = 10**6
+num1 = random.randint(1, N)
+num2 = random.randint(1, N)
+# Tính và in kết quả
+result = ucln_euclid(num1, num2)
+print(f"ƯCLN của {num1} và {num2} là: {result}")
+
+import math
+import random
+
+def experiment(N, num_trials):
+    count = 0
+    for i in range(0, N+1):
+        num1 = random.randint(1, N)
+        num2 = random.randint(1, N)
+        if math.gcd(num1, num2) == 1:
+            count += 1
+    return count / num_trials
+
+num_trials = 10**6
+N = 10**6
+P = experiment(N, num_trials)
+print("Xác suất P =", P)
